75762090971be499ea9a0f61f57e87feba0faa43
bahachammakhi <bahachammakhi25@gmail.com>
2020-03-15 00:42:42 +0100

    Signup Screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,24 +1,55 @@
 import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { ScrollView } from 'react-native-gesture-handler';
 import * as WebBrowser from 'expo-web-browser';
 import { TextInput, Button } from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
 
 const ContainerTest = styled.View`
   padding: 50px;
 `;
-const LoginScreen = () => {
+
+const styles = StyleSheet.create({
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+  paddingForm: {
+    marginTop: '10%',
+  },
+});
+const LoginScreen = ({ navigation }) => {
   return (
     <ContainerTest>
       <TextInput
+        style={styles.paddingForm}
         selectionColor="red"
         type="outlined"
         label="Email"
         // value={this.state.text}
         // onChangeText={text => this.setState({ text })}
       />
-      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+      <TextInput
+        style={styles.paddingForm}
+        selectionColor="red"
+        type="outlined"
+        label="Password"
+        // value={this.state.text}
+        // onChangeText={text => this.setState({ text })}
+      />
+      <Button
+        onPress={() => {
+          console.log('pressed2');
+        }}
+        style={styles.paddingForm}
+        mode="contained"
+        onPress={() => navigation.navigate('Root')}
+      >
         Press me
       </Button>
     </ContainerTest>
